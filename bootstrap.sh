68bfaf53e7c8b4659c96cdc3fe9c5909d91c7826
Piotr Szwarc <quarcu@gmail.com>
2015-03-06 15:16:07 +0000

    Symlink vim and vimrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,3 +8,7 @@ setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
     ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
+
+ln -s "${ZDOTDIR:-$HOME}"/dotfiles/.vim "${ZDOTDIR:-$HOME}"/.vim
+ln -s "${ZDOTDIR:-$HOME}"/dotfiles/.vimrc "${ZDOTDIR:-$HOME}"/.vimrc
+
