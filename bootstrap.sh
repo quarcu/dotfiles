edaa78d83d7619e2d758c56a3b8a710659e2def2
Piotr Szwarc <quarcu@gmail.com>
2015-03-06 08:06:49 +0000

    Update on bootstrap symlinks.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-current_dir=`pwd`
-ln -s $current_dir ~/dotfiles
+ln -s "${ZDOTDIR:-$HOME}"/Projects/dotfiles "${ZDOTDIR:-$HOME}"/dotfiles
+ln -s "${ZDOTDIR:-$HOME}"/dotfiles/.zprezto "${ZDOTDIR:-$HOME}"/.zprezto
 
 setopt EXTENDED_GLOB
 
-for rcfile in "${ZDOTDIR:-$HOME}"/dotfiles/.zprezto/runcoms/^README.md(.N); do
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
     ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
